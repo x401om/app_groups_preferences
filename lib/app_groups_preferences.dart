e01b2f170eab214139c662957d6dd7b5a92ba8ad
Alexey Goncharov <x401om@gmail.com>
2019-12-11 03:04:54 +0300

    setDouble

--- lib/app_groups_preferences.dart
+++ lib/app_groups_preferences.dart
@@ -1,13 +1,28 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 
 class AppGroupsPreferences {
-  static const MethodChannel _channel =
-      const MethodChannel('app_groups_preferences');
+  static const MethodChannel _channel = const MethodChannel('app_groups_preferences');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static void setDouble(String group, String key, double value) {
+    if (!Platform.isIOS) return;
+
+    final params = Map<String, dynamic>();
+    params['group'] = group;
+    params['key'] = key;
+    if (value != null) {
+      params['value'] = value;
+    } else {
+      params['remove'] = true;
+    }
+
+    _channel.invokeMethod('setDouble', params);
   }
 }
